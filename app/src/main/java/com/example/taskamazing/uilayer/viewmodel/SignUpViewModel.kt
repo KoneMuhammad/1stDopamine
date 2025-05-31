7fe02c5b0ab42d85489529a3dd61286675fbe2bd
ProgrammingPrograms <muhammadkone@tuta.com>
2025-05-31 16:10:49 -0400

    feat(task): implement TaskViewModel for combining taskMessage  and weather  data

--- app/src/main/java/com/example/taskamazing/uilayer/viewmodel/SignUpViewModel.kt
+++ app/src/main/java/com/example/taskamazing/uilayer/viewmodel/SignUpViewModel.kt
@@ -1,4 +1,53 @@
 package com.example.taskamazing.uilayer.viewmodel
 
-class SignUpViewModel {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.taskamazing.datalayer.domainmodel.ModelUser
+import com.example.taskamazing.datalayer.repository.supabaserepository.SupabaseUserRepository
+import com.example.taskamazing.errorhandler.ErrorHandleObject
+import com.example.taskamazing.errorhandler.errorHandleMessage
+import io.ktor.client.plugins.ClientRequestException
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import okio.IOException
+import javax.inject.Inject
+
+data class SignUpUiState(
+    val errorMessage: List<ErrorHandleObject> = emptyList(),
+    val loading: Boolean = false,
+    val success: Boolean = false
+)
+class SignUpViewModel@Inject constructor(private val supabaseUserRepository: SupabaseUserRepository):ViewModel() {
+
+    private val _SignUpUiState = MutableStateFlow(SignUpUiState())
+    val signUpUiState: StateFlow<SignUpUiState> = _SignUpUiState.asStateFlow()
+
+    fun createNewUser(email: String, password: String, user: ModelUser) {
+        viewModelScope.launch {
+            _SignUpUiState.update {
+                it.copy(loading = true)
+            }
+            try {
+                supabaseUserRepository.addNewUser(email,password)
+                supabaseUserRepository.upsertNewUser(user)
+                _SignUpUiState.update {
+                    it.copy(success = true)
+                }
+
+            }
+            catch (e: ClientRequestException){
+                _SignUpUiState.update {
+                    it.copy(errorMessage = errorHandleMessage(e))
+                }
+            }
+            catch (e: IOException){
+                _SignUpUiState.update {
+                    it.copy(errorMessage = errorHandleMessage(e))
+                }
+            }
+        }
+    }
 }
--- app/src/main/java/com/example/taskamazing/uilayer/viewmodel/TaskViewModel.kt
+++ app/src/main/java/com/example/taskamazing/uilayer/viewmodel/TaskViewModel.kt
@@ -1,4 +1,61 @@
 package com.example.taskamazing.uilayer.viewmodel
 
-class TaskViewModel {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.taskamazing.datalayer.dto.ModelDTOTaskMessage
+import com.example.taskamazing.datalayer.dto.ModelDTOTaskWeatherBackground
+import com.example.taskamazing.datalayer.repository.supabaserepository.SupabaseTaskMessageRepository
+import com.example.taskamazing.datalayer.repository.supabaserepository.SupabaseTaskWeatherRepository
+import com.example.taskamazing.errorhandler.ErrorHandleObject
+import com.example.taskamazing.errorhandler.errorHandleMessage
+import io.ktor.client.plugins.ServerResponseException
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+
+data class TaskUiState(
+    val modelTaskWeather: List<ModelDTOTaskWeatherBackground> = emptyList(),
+    val modelTaskMessage: List<ModelDTOTaskMessage> = emptyList(),
+    val message: List<ErrorHandleObject> = emptyList()
+)
+
+//addd a dark/lighttheme preference in top right of screen
+class TaskViewModel@Inject constructor(private val supabaseTaskMessageRepository: SupabaseTaskMessageRepository,
+    private val supabaseTaskWeatherRepository: SupabaseTaskWeatherRepository): ViewModel() {
+
+        //return  type viewmodel
+    private val _uiState = MutableStateFlow(TaskUiState())
+    val uiState:StateFlow<TaskUiState> = _uiState.asStateFlow()
+
+    fun swipeForNextCard(ids: Set<Long>){
+        viewModelScope.launch {
+            try {
+                combine(supabaseTaskWeatherRepository.getMultipleTaskWeather(ids),
+                    supabaseTaskMessageRepository.getMultipleTaskMessage(ids)){
+                    taskWeather, taskMessage -> _uiState.update {
+                        it.copy(
+                            modelTaskWeather = taskWeather,
+                            modelTaskMessage = taskMessage
+                        )
+                }
+                }
+
+            }catch (e: ServerResponseException){
+                _uiState.update {
+                    it.copy(message = errorHandleMessage(e))
+                }
+            }
+            catch (e: Exception){
+                _uiState.update {
+                    it.copy(message = errorHandleMessage(e))
+                }
+
+            }
+        }
+    }
 }
